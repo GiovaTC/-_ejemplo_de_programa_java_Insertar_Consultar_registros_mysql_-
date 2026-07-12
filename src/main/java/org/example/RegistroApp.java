15849d7f1b891ddbfa679e9d9c8bd2fd6d0e23ef
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-12 15:13:00 -0500

    commit - RegistroApp .

--- src/main/java/org/example/RegistroApp.java
+++ src/main/java/org/example/RegistroApp.java
@@ -10,5 +10,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class RegistroApp {
+    private static final String URL = "jdbc:mysql://localhost:3306/mi_base_datos";
+    private static final String USER = "root"; //cambia segun tu configuracion!
+    private static final String PASSWORD = "Tapiero123";
 
-}   
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("REGISTRO DE DATOS");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        placeComponents(panel);
+
+        frame.setVisible(true);
+    }
+
+    private static void placeComponents(JPanel panel) {
+    }   
+
+}
