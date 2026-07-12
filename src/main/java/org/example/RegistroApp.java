bd58fccd0684c0d1c31f7967c9db0d56909a9bd8
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-12 15:08:35 -0500

    commit - RegistroApp .

---
+++ src/main/java/org/example/RegistroApp.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class RegistroApp {
+
+}   
